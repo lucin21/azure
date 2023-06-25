164387a56d4613e8a11d2c900116bde2ef5e61fc
LucinHuseinPerezGuti <lucin.perez@testingit.com.mx>
2023-06-25 10:29:30 -0600

    git

---
+++ test_001.py
@@ -0,0 +1,15 @@
+import unittest
+import time
+
+class Terceros(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_producto_envio_domicilio1(self):
+        self.assertEquals(2,2, "Fallo la prueba 1")
+
+    def test_producto_envio_domicilio2(self):
+        self.assertEquals(2,2, "Fallo la prueba 2")
+
+    def test_producto_envio_domicilio3(self):
+        self.assertEquals(2,2, "Fallo la prueba 3")
